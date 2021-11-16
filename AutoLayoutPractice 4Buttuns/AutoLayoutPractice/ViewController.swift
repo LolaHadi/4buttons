77abaffbbece935b1daffc5a05e08caa76d09a85
LolaHadi <92252764+LolaHadi@users.noreply.github.com>
2021-11-16 23:54:42 +0300

    ..

---
+++ AutoLayoutPractice 4Buttuns/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,385 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Tariq Almazyad on 11/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+
+    
+   
+    let clickMeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("1", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
+        return button
+    }()
+    
+    let clickMeButton2: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("2", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .yellow
+        button.setDimensions(height: 50, width: 50)
+
+        return button
+    }()
+
+    let clickMeButton3: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("3", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        button.setDimensions(height: 50, width: 50)
+        return button
+    }()
+
+    let clickMeButton4: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("4", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .green
+        button.setDimensions(height: 50, width: 50)
+        return button
+    }()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(clickMeButton)
+        clickMeButton.translatesAutoresizingMaskIntoConstraints = false
+        let horizontalConstraint = NSLayoutConstraint(item: clickMeButton, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: -25)
+        let verticalConstraint = NSLayoutConstraint(item: clickMeButton, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: -25)
+        let widthConstraint = NSLayoutConstraint(item: clickMeButton, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: 50)
+        let heightConstraint = NSLayoutConstraint(item: clickMeButton, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: 50)
+        view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+
+
+        view.addSubview(clickMeButton2)
+        clickMeButton2.anchor(left: clickMeButton.rightAnchor)
+        NSLayoutConstraint(item: clickMeButton2, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: -25).isActive = true
+
+        view.addSubview(clickMeButton3)
+        clickMeButton3.anchor(top: clickMeButton.bottomAnchor)
+        clickMeButton3.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: clickMeButton3, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: -25).isActive = true
+
+        view.addSubview(clickMeButton4)
+        clickMeButton4.anchor(left: clickMeButton3.rightAnchor)
+        clickMeButton4.anchor(top: clickMeButton2.bottomAnchor)
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+public struct AnchoredConstraints {
+    public var top, leading, bottom, trailing, width, height: NSLayoutConstraint?
+}
+
+
+
+extension UIView {
+    
+    /// to round the desired corner NOTE: you must put it in the layoutSubviews() example corners: [.topLeft, .topRight], radius: 50)
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        if #available(iOS 11, *) {
+            self.clipsToBounds = true
+            self.layer.cornerRadius = radius
+            var masked = CACornerMask()
+            if corners.contains(.topLeft) { masked.insert(.layerMinXMinYCorner) }
+            if corners.contains(.topRight) { masked.insert(.layerMaxXMinYCorner) }
+            if corners.contains(.bottomLeft) { masked.insert(.layerMinXMaxYCorner) }
+            if corners.contains(.bottomRight) { masked.insert(.layerMaxXMaxYCorner) }
+            self.layer.maskedCorners = masked
+        }
+        else {
+            let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            layer.mask = mask
+        }
+    }
+    
+    @discardableResult
+    func applyGradient(colours: [UIColor]) -> CAGradientLayer {
+        return self.applyGradient(colours: colours, locations: nil)
+    }
+    
+    @discardableResult
+    func applyGradient(colours: [UIColor], locations: [NSNumber]?) -> CAGradientLayer {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colours.map { $0.cgColor }
+        gradient.locations = locations
+        self.layer.insertSublayer(gradient, at: 0)
+        return gradient
+    }
+    
+    func setGradientBackground(colorTop: UIColor, colorBottom: UIColor){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorBottom.cgColor, colorTop.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.locations = [0, 1]
+        gradientLayer.frame = bounds
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    func customAddSubViews(_ views: UIView...  ){
+        views.forEach{addSubview($0)}
+    }
+    
+    func setGradiantBGColor(colorOne:UIColor, colorTwo:UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 2.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func centerX(inView view: UIView, topAnchor: NSLayoutYAxisAnchor? = nil,
+                 paddingTop: CGFloat = 0, constant: CGFloat = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: constant).isActive = true
+        if let top = topAnchor{
+            anchor(top: top, paddingTop: paddingTop)
+        }
+        
+    }
+    
+    
+    func centerY(inView view: UIView, leftAnchor: NSLayoutXAxisAnchor? = nil,
+                 paddingLeft: CGFloat = 0, constant: CGFloat = 0) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant).isActive = true
+        
+        if let left = leftAnchor {
+            anchor(left: left, paddingLeft: paddingLeft)
+        }
+    }
+    
+    func setDimensions(height: CGFloat, width: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+    func setHeight(height: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+    
+    func setWidth(width: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+    
+    
+    
+    @discardableResult
+    open func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) -> AnchoredConstraints {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+        
+        if let top = top {
+            anchoredConstraints.top = topAnchor.constraint(equalTo: top, constant: padding.top)
+        }
+        
+        if let leading = leading {
+            anchoredConstraints.leading = leadingAnchor.constraint(equalTo: leading, constant: padding.left)
+        }
+        
+        if let bottom = bottom {
+            anchoredConstraints.bottom = bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom)
+        }
+        
+        if let trailing = trailing {
+            anchoredConstraints.trailing = trailingAnchor.constraint(equalTo: trailing, constant: -padding.right)
+        }
+        
+        if size.width != 0 {
+            anchoredConstraints.width = widthAnchor.constraint(equalToConstant: size.width)
+        }
+        
+        if size.height != 0 {
+            anchoredConstraints.height = heightAnchor.constraint(equalToConstant: size.height)
+        }
+        
+        [anchoredConstraints.top, anchoredConstraints.leading, anchoredConstraints.bottom, anchoredConstraints.trailing, anchoredConstraints.width, anchoredConstraints.height].forEach{ $0?.isActive = true }
+        
+        return anchoredConstraints
+    }
+    
+    @discardableResult
+    open func fillSuperview(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+        translatesAutoresizingMaskIntoConstraints = false
+        let anchoredConstraints = AnchoredConstraints()
+        guard let superviewTopAnchor = superview?.topAnchor,
+              let superviewBottomAnchor = superview?.bottomAnchor,
+              let superviewLeadingAnchor = superview?.leadingAnchor,
+              let superviewTrailingAnchor = superview?.trailingAnchor else {
+                  return anchoredConstraints
+              }
+        
+        return anchor(top: superviewTopAnchor, leading: superviewLeadingAnchor, bottom: superviewBottomAnchor, trailing: superviewTrailingAnchor, padding: padding)
+    }
+    
+    @discardableResult
+    open func fillSuperviewSafeAreaLayoutGuide(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+        let anchoredConstraints = AnchoredConstraints()
+        if #available(iOS 11.0, *) {
+            guard let superviewTopAnchor = superview?.safeAreaLayoutGuide.topAnchor,
+                  let superviewBottomAnchor = superview?.safeAreaLayoutGuide.bottomAnchor,
+                  let superviewLeadingAnchor = superview?.safeAreaLayoutGuide.leadingAnchor,
+                  let superviewTrailingAnchor = superview?.safeAreaLayoutGuide.trailingAnchor else {
+                      return anchoredConstraints
+                  }
+            return anchor(top: superviewTopAnchor, leading: superviewLeadingAnchor, bottom: superviewBottomAnchor, trailing: superviewTrailingAnchor, padding: padding)
+            
+        } else {
+            return anchoredConstraints
+        }
+    }
+    
+    open func centerInSuperview(size: CGSize = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+        
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+    
+    open func centerXToSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+    }
+    
+    open func centerYToSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+    }
+    
+    @discardableResult
+    open func constrainHeight(_ constant: CGFloat) -> AnchoredConstraints {
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+        anchoredConstraints.height = heightAnchor.constraint(equalToConstant: constant)
+        anchoredConstraints.height?.isActive = true
+        return anchoredConstraints
+    }
+    
+    @discardableResult
+    open func constrainWidth(_ constant: CGFloat) -> AnchoredConstraints {
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+        anchoredConstraints.width = widthAnchor.constraint(equalToConstant: constant)
+        anchoredConstraints.width?.isActive = true
+        return anchoredConstraints
+    }
+    
+    open func setupShadow(opacity: Float = 0, radius: CGFloat = 0,
+                          offset: CGSize = .zero, color: UIColor = .black) {
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        clipsToBounds = true
+        layer.masksToBounds = false
+    }
+    
+    open func setupShadowAndBorder(opacity: Float = 0, radius: CGFloat = 0,
+                                   offset: CGSize = .zero, color: UIColor = .black,
+                                   borderColor: UIColor, borderWidth: CGFloat) {
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        clipsToBounds = true
+        layer.masksToBounds = false
+        layer.borderColor = borderColor.cgColor
+        layer.borderWidth = borderWidth
+    }
+    
+    
+    convenience public init(backgroundColor: UIColor = .clear) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+    }
+    
+}
+
